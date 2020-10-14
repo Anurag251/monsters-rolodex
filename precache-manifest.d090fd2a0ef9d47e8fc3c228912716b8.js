b0882a5f59b420389a0d81c8011290e57019d7f2
Anurag251 <rana.anurag12345@gmail.com>
2020-10-14 13:19:35 +0545

    Updates

---
+++ precache-manifest.d090fd2a0ef9d47e8fc3c228912716b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74de7f45baf749cc61acec6dec392dfb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2ef12e5d7be7cae9d6dc",
+    "url": "/monsters-rolodex/static/css/main.ea8b84b6.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef12e5d7be7cae9d6dc",
+    "url": "/monsters-rolodex/static/js/main.a420932a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
